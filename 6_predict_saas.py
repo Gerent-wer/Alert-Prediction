cd178ae7ac32945f27e09fb662caaa3e0b6586b1
Gerent-wer <dima.iva04@gmail.com>
2023-05-03 14:34:31 +0300

    Update 6_predict_saas.py

--- 6_predict_saas.py
+++ 6_predict_saas.py
@@ -15,6 +15,7 @@
 
 # import libraries
 import datetime
+import os
 import pickle
 import nltk
 import re
@@ -36,9 +37,6 @@ from utils import get_weather
 from utils import text_processing
 from flask import Flask, jsonify, request
 
-# nltk.download('stopwords')
-# nltk.download('punkt')
-# nltk.download('wordnet')
 
 app = Flask(__name__)
 
@@ -51,132 +49,18 @@ def get_alert_predict():
     return predict
 
 
-# def alert_predict():
-#
-# ### get and preprocess ISW files
-#
-# # get article from yesterday
-# today = datetime.date.today()
-# yesterday = today - datetime.timedelta(days=1)
-# yesterday_day = yesterday.day
-# yesterday_month = yesterday.month
-# yesterday_year = yesterday.year
-#
-# file = text_processing.get_article_from_yesterday(yesterday_day, yesterday_month, yesterday_year)
-# data = text_processing.read_html(file)
-#
-# # articles preprocess
-# def preprocess_all_text(data):
-#     pattern = "\[(\d+)\]"
-#     data['main_html_v1'] = data['main_html'].apply(lambda x: re.sub(pattern, "", str(x)))
-#     data['main_html_v2'] = data['main_html_v1'].apply(lambda x: re.sub(r'http(\S+.*\s)', "", x))
-#     data['main_html_v3'] = data['main_html_v2'].apply(lambda x: re.sub(r'2022|2023|©2022|©2023|\xa0|\n', "", x))
-#     data['main_html_v4'] = data['main_html_v3'].apply(lambda x: BeautifulSoup(x).text)
-#     data['main_html_v5'] = data['main_html_v4'].apply(lambda x: text_processing.remove_names_and_dates(x))
-#
-#     return data
-#
-# data_preprocessed = preprocess_all_text(data)    # %%
-# data_preprocessed = data_preprocessed.drop(['main_html_v1', 'main_html_v2', 'main_html_v3', 'main_html_v4'], axis=1)
-#
-# data_preprocessed['report_text_lemm'] = data_preprocessed['main_html_v5'].apply(
-#     lambda x: text_processing.preprocess(x, "lemm"))
-# data_preprocessed['report_text_stemm'] = data_preprocessed['main_html_v5'].apply(
-#     lambda x: text_processing.preprocess(x, "stemm"))
-#
-# docs = data_preprocessed['report_text_lemm'].tolist()
-#
-# ### ISW vectorize
-#
-# cv = CountVectorizer()
-# word_count_vector = cv.fit_transform(docs)
-# tfidf_transformer = TfidfTransformer(smooth_idf=True, use_idf=True)
-# tfidf_transformer.fit(word_count_vector)
-# tf_idf_vector = tfidf_transformer.transform(word_count_vector)
-# feature_names = cv.get_feature_names_out()
-#
-# data_preprocessed['keywords'] = data_preprocessed['report_text_stemm'].apply(
-#     lambda x: text_processing.convert_doc_to_vector(x, feature_names, tf_idf_vector))
-#
-# #### Part of script: Final preprocessing
-#
-# data_preprocessed["date_datetime"] = pd.to_datetime(data_preprocessed["date"])
-# data_preprocessed['date_tomorrow_datetime'] = data_preprocessed['date_datetime'].apply(
-#     lambda x: x + datetime.timedelta(days=1))
-# data_preprocessed = data_preprocessed.rename(columns={"date_datetime": "report_date"})
-#
-# data_vectorised = tf_idf_vector.toarray()
-# vectors_df = pd.DataFrame(data_vectorised)
-# vectors_df['date'] = pd.to_datetime(today)
-#
-# df_isw_short = data_preprocessed[['date', 'report_text_lemm', 'keywords', 'date_tomorrow_datetime']]
-#
-# ### Predict
-#
-# tfidf = pickle.load(open("models/tfidf_transformer_v1.pkl", "rb"))
-# cv = pickle.load(open("models/count_vectorizer_v1.pkl", "rb"))
-# model = pickle.load(open("models/training_models/4_logreg_1.5f.pkl", "rb"))
-#
-# cities = ['Vinnytsia', 'Simferopol', 'Lutsk', 'Dnipro', 'Donetsk', 'Zhytomyr', 'Uzhgorod', 'Zaporozhye',
-#           'Ivano-Frankivsk', 'Kyiv',
-#           'Kropyvnytskyi', 'Luhansk', 'Lviv', 'Mykolaiv', 'Odesa', 'Poltava', 'Rivne', 'Sumy', 'Ternopil',
-#           'Kharkiv', 'Kherson',
-#           'Khmelnytskyi', 'Cherkasy', 'Chernivtsi', 'Chernihiv']
-#
-# date = datetime.datetime.now(pytz.timezone('Europe/Kyiv'))
-# current_time = str(pd.Timestamp.now().replace(microsecond=0))
-# result = {"last_prediction_time": current_time}
-#
-# for city in cities:
-#
-#     df_weather_final = get_weather.get_weather_for_12_hours(city, date)
-#
-#     # merge
-#     df_weather_final['key'] = 1
-#     df_isw_short['key'] = 1
-#     df_all = df_weather_final.merge(df_isw_short, how='left', left_on='key', right_on='key')
-#
-#     # drop
-#     to_drop = ['key', 'date', 'date_tomorrow_datetime', 'keywords', 'report_text_lemm']
-#     if 'sunrise' in df_all.columns:
-#         exceptions = ['sunset', 'sunrise']
-#         to_drop.extend(exceptions)
-#     df_weather_matrix_v1 = df_all.drop(to_drop, axis=1)
-#
-#     # final dataset
-#     df_weather_matrix_v1['Unnamed: 0'] = 0
-#     df_weather_matrix_v1 = df_weather_matrix_v1[
-#         ['Unnamed: 0', 'day_datetimeEpoch', 'day_tempmax', 'day_tempmin', 'day_temp', 'day_dew', 'day_humidity',
-#          'day_precip', 'day_precipcover', 'day_solarradiation', 'day_solarenergy', 'day_uvindex', 'day_moonphase',
-#          'hour_datetimeEpoch', 'hour_temp', 'hour_humidity', 'hour_dew', 'hour_precipprob', 'hour_snow',
-#          'hour_snowdepth', 'hour_windgust', 'hour_windspeed', 'hour_winddir', 'hour_pressure', 'hour_visibility',
-#          'hour_cloudcover', 'hour_severerisk', 'region_id']]
-#
-#     cv_vector_model = cv.transform(df_all['report_text_lemm'].values.astype('U'))
-#     TF_IDF_MODEL = tfidf.transform(cv_vector_model)
-#
-#     df_weather_matrix_v1_csr = scipy.sparse.csr_matrix(df_weather_matrix_v1)
-#     df_all_data_csr = scipy.sparse.hstack((df_weather_matrix_v1_csr, TF_IDF_MODEL), format='csr')
-#
-#     # predict
-#     predicted = model.predict(df_all_data_csr)
-#     predicted_list = predicted.tolist()
-#     current_time = pd.Timestamp.now()
-#     hours = []
-#
-#     for i in range(12):
-#         hour = date + datetime.timedelta(hours=i)
-#         hour_rounded = hour.replace(minute=0, second=0, microsecond=0)
-#         hours.append(hour_rounded.strftime('%Y-%m-%d %H:%M'))
-#
-#     result[city] = dict(zip(hours, predicted_list))
-#
-# # Save json
-# with open("data/results/predict.json", "w") as outfile:
-#     json.dump(result, outfile)
+def get_last_predict_time():
+    # elapsed since EPOCH in float
+    ti_m = os.path.getmtime("data/results/predict.json")
+
+    # Beatify time veiw
+    m_ti = time.strftime('%Y-%m-%d %H:%M')
+
+    return m_ti
 
 
 class InvalidUsage(Exception):
+
     status_code = 400
 
     def __init__(self, message, status_code=None, payload=None):
@@ -185,7 +69,6 @@ class InvalidUsage(Exception):
         if status_code is not None:
             self.status_code = status_code
         self.payload = payload
-
     def to_dict(self):
         rv = dict(self.payload or ())
         rv["message"] = self.message
@@ -210,10 +93,11 @@ def home_page():
 )
 def alarm_predict_endpoint():
 
-    start_dt = dt.datetime.now()
+    last_prediction_time = get_last_predict_time()
     predict = get_alert_predict()
+
     result = {
-        "last_prediction_time": start_dt.isoformat(),
+        "last_prediction_time": last_prediction_time,
         "regions_forecast": predict,
     }
 
